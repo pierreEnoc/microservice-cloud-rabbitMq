10cc6d9658318b840e95fbda351b1eab62f622a3
Pierre enoc <pierre.enoc@gympass.com>
2021-05-01 20:12:59 -0300

    project created

---
+++ cr-produto/src/main/java/com/pierre/crproduto/CrProdutoApplication.java
@@ -0,0 +1,13 @@
+package com.pierre.crproduto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrProdutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrProdutoApplication.class, args);
+	}
+
+}
