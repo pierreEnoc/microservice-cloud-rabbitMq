711e6bdfd2876d22f33a8226041821a7651aea0f
Pierre enoc <pierre.enoc@gympass.com>
2021-05-02 13:44:11 -0300

    ms-pagamento created

---
+++ ms-pagamento/src/test/java/com/pierre/pagamento/MsPagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pierre.pagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
